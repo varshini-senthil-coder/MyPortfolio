116b4236f6250965504a1c6f48176d04baf150f8
varshini <varshinisenthil2005@gmail.com>
2026-03-22 21:39:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Change this to your name
+document.getElementById("name").textContent = "Your Name";
+
+// Contact form alert
+function submitForm() {
+    alert("Message sent successfully!");
+    return false;
+}
